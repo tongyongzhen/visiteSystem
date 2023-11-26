3cff40dd0fec3fa9c72a7770e17f5d56abf7fee6
tongyongzhen <2569925723@qq.com>
2023-11-26 15:03:42 +0800

    最新

--- src/main/java/visite_system/demo/Mapper/VIP_AppointmentMapper.java
+++ src/main/java/visite_system/demo/Mapper/VIP_AppointmentMapper.java
@@ -1,7 +1,12 @@
 package visite_system.demo.Mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Param;
 import visite_system.demo.Entity.VipAppointment;
+import visite_system.demo.Entity.VipExamine;
+
+import java.util.List;
 
 public interface VIP_AppointmentMapper extends BaseMapper<VipAppointment> {
+     List<VipExamine> queryVipAppointmentByUserId(@Param("id") Long id);
 }
